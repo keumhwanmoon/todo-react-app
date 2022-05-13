e863e796175114d6fd7acbf9adc8daf75b821c39
jason.moon <hwanth85@gmail.com>
2022-05-13 23:17:43 +0900

    add todo,
    delete todo 기능 추가.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import Todo from './Todo';
+import AddTodo from './AddTodo';
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {id: "0", title: "Hello World 1", done: true},
+        {id: "1", title: "Hello World 2", done: false}
+      ]
+    }
+  }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+
+    item.id = `ID=${thisItems.length}`;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({items: thisItems});
+    console.log("items : ", this.state.items);
+  }
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const updatedItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items: updatedItems}, () => {
+      console.log("Updated Items : ", this.state.items);
+    });
+  }
+
+  render() {
+    let todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete}/>
+          ))}
+        </List>
+      </Paper>
+    )
+
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add}/>
+          {todoItems}
+        </Container>
+      </div>
+    )}  
 }
 
 export default App;
